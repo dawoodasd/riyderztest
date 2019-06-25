7af9b256c3d9323e9ef48fcb6284eee76f7e3f2c
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-06-25 19:47:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,16 +16,6 @@ client.on('ready', () => {
  });
  
  
- client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('.com')){
-      if(!message.member.hasPermission('MANAGE_MESSAGE'))
-        message.delete()
-    return message.reply(`**Don't Share Links Plz :x:**`)
-    }
-});
- 
- 
  
 
  
