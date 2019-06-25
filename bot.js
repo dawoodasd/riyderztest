da959b53b2403bd5a08d82020c0a1e56850468a7
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-06-25 19:07:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,55 +4,16 @@ const prefix = '!'
 
  
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- client.on('ready', function(){
-console.log(`Logged in as ${client.user.tag}!`);    
-   var large = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293']; // صور الكبيرة
- var small = ['483055660209012736','480169573530861578','483055655800930315'];  // صور الصغيرة
-    setInterval(function (){  
-    client.user.setPresence({
-  status: 'dnd', 
- game: { 
-    type: 1,
-    name: 'new stream is come ',
-     url: 'https://www.twitch.tv/n3k4a',
-     state: `new stream code `,
-    application_id: '532298006951624745',  
-     assets: {
-        small_text: ' n3k4a is one! ' ,
-        large_text: `I'm n3k4a :)` },
-    large_image:  `${large[Math.floor(Math.random() * large.length)]}`,
-    small_image:   `${small[Math.floor(Math.random() * small.length)]}`,
-    }
-  
-    });
-    }, 5000);
-});
- 
+client.on('ready', () => {
+    console.log(`~~~~~~~~~~~~~~~~~`);
+    console.log(`Logging into Discord`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`on  ${client.guilds.size} Servers `);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`#Riyderz TesT |!Help`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+ });
  
  
  
