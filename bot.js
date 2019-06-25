631349f66bcd5ca3e4ecd67ed4b7331ae9e70ac3
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-06-25 19:48:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,29 @@ client.on('ready', () => {
  });
  
  
- 
+ client.on('message', message => {
+    if (message.content.includes('disc ord.gg')){      //شيل المسافه
+                        if(!message.channel.guild) return message.reply ('')
+                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
+       message.channel.send('kick <@' + message.author.id + '>')
+       message.delete() 
+       }
+    }
+          if (message.content.startsWith("kick ")) {
+             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
+             var member= message.mentions.members.first();
+             member.ban().then((member) => {
+                 message.channel.sendMessage("", {embed: {
+                 author: {
+                 },
+                 title: 'بسبب النشر ' + member.displayName + ' تم طرد',
+                 color: 490101,
+                 }
+               });
+           }
+         ) 
+       }
+   });
 
  
  
